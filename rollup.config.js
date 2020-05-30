20b32dc5a150076aaa4373c0932e3c2a407cab51
Niels Bik <nielsrowinbik@gmail.com>
2020-05-30 23:55:15 +0200

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
+
+import pkg from "./package.json";
+
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            banner: "#!/usr/bin/env node",
+            file: pkg.main,
+            format: "cjs",
+        },
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [json(), typescript()],
+};
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { program } from "commander";
+import figlet from "figlet";
+// @ts-ignore ref: https://github.com/patorjk/figlet.js/issues/52
+import standard from "figlet/importable-fonts/Standard";
+
+import { init } from "node-blue";
+
+import { description, version } from "../package.json";
+
+const name = "Node-BLUE";
+
+program.version(version);
+program.description(description);
+
+// @ts-ignore ref: https://github.com/patorjk/figlet.js/issues/52
+figlet.parseFont("Standard", standard);
+console.log(figlet.textSync(name));
+
+program
+    .command("start [nodes]")
+    .description(`Start ${name}`)
+    .option("-h, --host <host>", "your Home Assistant instance's host")
+    .option("-P, --path <path>", "path to the Websocket API", "/api/websocket")
+    .option("-p, --port <port>", "your Home Assistant instance's port", "8123")
+    .option("-s, --secure", "use a secure connection")
+    .option("-t, --token <token>", "a long-lived access token")
+    .action(init);
+
+program.parse(process.argv);
